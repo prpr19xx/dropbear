69599efde8d8c68d5650d0513f8735c4b4daee19
prpr19xx <58330423+prpr19xx@users.noreply.github.com>
2024-04-13 20:58:15 +0100

    Local patches

--- src/svr-chansession.c
+++ src/svr-chansession.c
@@ -608,7 +608,7 @@ static int sessionpty(struct ChanSess * chansess) {
 		dropbear_exit("Out of memory"); /* TODO disconnect */
 	}
 
-	pw = getpwnam(ses.authstate.pw_name);
+	pw = my_getpwnam(ses.authstate.pw_name);
 	if (!pw)
 		dropbear_exit("getpwnam failed after succeeding previously");
 	pty_setowner(pw, chansess->tty);
@@ -1011,6 +1011,7 @@ static void execchild(const void *user_data) {
 	addnewvar("LOGNAME", ses.authstate.pw_name);
 	addnewvar("HOME", ses.authstate.pw_dir);
 	addnewvar("SHELL", get_user_shell());
+	addnewvar("LD_LIBRARY_PATH", "/mod/lib:/lib");
 	if (getuid() == 0) {
 		addnewvar("PATH", DEFAULT_ROOT_PATH);
 	} else {
