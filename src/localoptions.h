329e0e45fa7d6e9681a980716a6a48f12e4afdc1
prpr19xx <58330423+prpr19xx@users.noreply.github.com>
2024-04-13 21:00:06 +0100

    Add local options

---
+++ src/localoptions.h
@@ -0,0 +1,15 @@
+#define DSS_PRIV_FILENAME "/mod/etc/dropbear/dropbear_dss_host_key"
+#define RSA_PRIV_FILENAME "/mod/etc/dropbear/dropbear_rsa_host_key"
+#define ECDSA_PRIV_FILENAME "/mod/etc/dropbear/dropbear_ecdsa_host_key"
+#define ED25519_PRIV_FILENAME "/mod/etc/dropbear/dropbear_ed25519_host_key"
+#define SFTPSERVER_PATH "/mod/sbin/sftp-server"
+#define DROPBEAR_PATH_SSH_PROGRAM "/mod/bin/dbclient"
+#define DEFAULT_PATH "/mod/bin:/usr/bin:/bin"
+
+#define DO_MOTD 1
+#define INETD_MODE 0
+#define DROPBEAR_X11FWD 0
+#define DROPBEAR_SVR_AGENTFWD 0
+#define DROPBEAR_CLI_AGENTFWD 0
+#define DROPBEAR_CLI_NETCAT 0
+
